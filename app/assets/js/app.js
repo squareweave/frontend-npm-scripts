679098483158036117fadbf153ce2c5717d5d613
Tom Gordon <tom.gordon@squareweave.com.au>
2017-10-23 16:25:24 +1100

    Fix indents

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -2,10 +2,11 @@
 import jQuery from "jquery";
 import Foundation from "foundation-sites";
 
-(function($) {
+(function ($) {
     console.log(`jQuery version: ${$.fn.jquery}`);
     console.log(`Foundation version: ${Foundation.version}`);
-    $.get("/assets/images/sprite/symbol/svg-sprite.svg", function(data) {
+
+    $.get("/assets/images/sprite/symbol/svg-sprite.svg", function (data) {
         var div = document.createElement("div");
         div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
         document.body.insertBefore(div, document.body.childNodes[0]);
